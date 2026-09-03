494b243275fb0532e3f3a33b122a22aa5c49cea2
李子豪 <1415829227@qq.com>
2026-09-03 09:00:00 +0800

    修复：数据库迁移修复

---
+++ uStudy-backend/alembic/versions/merge_heads_v1_unify_current_heads.py
@@ -0,0 +1,23 @@
+"""merge current alembic heads
+
+Revision ID: merge_heads_v1
+Revises: daily_study_v1, folders_v1, message_response_status_v1
+Create Date: 2026-03-23 00:10:00.000000
+
+"""
+from typing import Sequence, Union
+
+
+# revision identifiers, used by Alembic.
+revision: str = "merge_heads_v1"
+down_revision: Union[str, Sequence[str], None] = ("daily_study_v1", "folders_v1", "message_response_status_v1")
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
